217202b213d812f07596d9cacb0cac00ab15dc8c
JennaMMurphy <blue-marie5@hotmail.com>
2020-05-08 12:30:45 -0600

    added mouseover that gives a description of dog

--- src/components/Dog.js
+++ src/components/Dog.js
@@ -1,19 +1,30 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function Dogs ({ dogs }) {
-    return(
-        <div className="dogs">
-            <div className="dogs__name">
-                <p>{dogs.name}</p>
-            </div>
-            <div className="dogs__meta">
-                <span>{dogs.breed}</span>
-            </div>
-            <div className="dogs__image">
-                <img src={dogs.imageUrl} alt='Image not available' />
-            </div>
-        </div>
-    )
+function Dogs({ dogs }) {
+  const [isShown, setIsShown] = useState(false);
+
+  return (
+    <div className="dogs">
+      <div className="dogs__name">
+        <p>{dogs.name}</p>
+      </div>
+      <div className="dogs__meta">
+        <span>{dogs.breed}</span>
+      </div>
+      <div
+        className="dogs__image "
+        onMouseEnter={() => setIsShown(true)}
+        onMouseLeave={() => setIsShown(false)}
+      >
+        <img src={dogs.imageUrl} alt="Image not available" />
+        {isShown && (
+          <div className="dogs__mouseover">
+            <h1 className="imageText">{dogs.description}</h1>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Dogs
+export default Dogs;
